a94b91d99f77608b79fb58b8d966fb03817c8a01
Richard King <rkingucla@ymail.com>
2013-07-15 17:58:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StatusBoard::Application.config.secret_key_base = 'e6a5763b5f004ccf899dad2ef68eff9b3d3f47d845688b81a0c540a98c9cd18deee94d33bad36052ac9423a4ce8d6a79b429592a5e9bc0dbd16f4f00d8d85b79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StatusBoard::Application.config.session_store :cookie_store, key: '_status_board_session'
